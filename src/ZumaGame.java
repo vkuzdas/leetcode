159405d0b7656725855ffd9db5f78f4ac0903454
vkuzdas <kuzdavoj@fel.cvut.cz>
2024-08-11 14:09:58 +0200

    Zuma collapse improvement

--- src/ZumaGame.java
+++ src/ZumaGame.java
@@ -1,7 +1,4 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Queue;
+import java.util.*;
 
 // 488. Zuma Game, HARD
 // https://leetcode.com/problems/zuma-game/description/
@@ -12,14 +9,19 @@ public class ZumaGame {
 
     public static void main(String[] args) {
 //        System.out.println("hello".substring(0,2) + "hello".substring(3));
-//        System.out.println(findMinStep("WRRBBW", "RB"));
-//        System.out.println(findMinStep("WWRRBBWW", "WRBRW"));
-//        System.out.println(findMinStep("G", "GGGGG"));
         Solution s = new Solution();
+//        System.out.println(s.findMinStep("WRRBBW", "RB"));
+//        System.out.println(s.findMinStep("WWRRBBWW", "WRBRW"));
+//        System.out.println(findMinStep("G", "GGGGG"));
 
 //        System.out.println(s.findMinStep("WWGWGW", "GWBWR")); // 3
 //        System.out.println(s.findMinStep("WWGWGW", "GWBWR")); // 3
-        System.out.println(s.findMinStep("RRWWRRBBRR", "WB")); // 2
+//        System.out.println(s.findMinStep("RRWWRRBBRR", "WB")); // 2
+//        System.out.println(s.findMinStep("RRGGBBYYWWRRGGBB", "RGBYW")); // 2
+        System.out.println(s.findMinStep("WWRRBBWW", "WRBRW")); // 2
+
+//        System.out.println(s.collapseBoard("ABBBAACCC"));
+//        System.out.println(s.collapseBoard("WWWW"));
     }
 
 
@@ -33,9 +35,27 @@ class Solution {
         String hand;
         int depth;
 
+        public GameState(String board, String hand, int depth) {
+            this.board = board;
+            this.hand = hand;
+            this.depth = depth;
+        }
         public GameState(String board, String hand) {
             this.board = board;
             this.hand = hand;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GameState gameState = (GameState) o;
+            return Objects.equals(board, gameState.board) && Objects.equals(hand, gameState.hand);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(board, hand);
         }
     }
 
@@ -43,24 +63,29 @@ class Solution {
 
 
     public int findMinStep(String board, String hand) {
-        // Collapse the root state
         board = collapseBoard(board);
+
         HashMap<Integer, GameState> stateMap = new HashMap<>();
+        HashSet<GameState> allStates = new HashSet<>();
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
         graph.add(new ArrayList<>()); // root nbrs
 
-        GameState root = new GameState(board, hand);
+        char[] chars = hand.toCharArray();
+        Arrays.sort(chars);
+        GameState root = new GameState(board, new String(chars));
         root.depth = 0;
         stateMap.put(0, root);
+        allStates.add(root);
 
         // start BFS
         Queue<Integer> Q = new ArrayDeque<>();
-
-
-        Q.add(0); // add root
+        Q.add(0);
         while (!Q.isEmpty()) {
             Integer curr = Q.poll();
-            ArrayList<Integer> nbrs = initNeighbors(curr, graph, stateMap);
+
+            genAllNeighbors(curr, graph, stateMap, allStates);
+            ArrayList<Integer> nbrs = graph.get(curr);
+
             for(Integer n : nbrs) {
                 if (stateMap.get(n).board.isEmpty()) {
                     return stateMap.get(n).depth;
@@ -73,68 +98,82 @@ class Solution {
 
     }
 
-    private String collapseBoard(String board) {
-
-        while (true) {
-            int prevLen = board.length();
-            board = board.replaceAll("RRR+", "");
-            board = board.replaceAll("YYY+", "");
-            board = board.replaceAll("BBB+", "");
-            board = board.replaceAll("GGG+", "");
-            board = board.replaceAll("WWW+", "");
-            int newLen = board.length();
-
-            if (newLen == prevLen) break;
-        }
-
-        return board;
-    }
-
-    // given current, graph and map
-    // analyze board and hand, based on that generate new states, put them into graph and map
-    // TODO: sort hand and add states to hashSet, then you wont have duplicates
-    private ArrayList<Integer> initNeighbors(Integer curr, ArrayList<ArrayList<Integer>> graph, HashMap<Integer, GameState> stateMap) {
+    /**
+     * Generate all neighbors by placing every char from hand on each possible position on the board
+     */
+    private void genAllNeighbors(Integer curr, ArrayList<ArrayList<Integer>> graph, HashMap<Integer, GameState> stateMap, HashSet<GameState> allStates) {
         GameState s = stateMap.get(curr);
-        ArrayList<Integer> nbrs = new ArrayList<>();
 
-        // pro kazdej char v ruce najdi kazdej vyskyt dvou po sobe jdoucich techto charu
-        // a taky jeden char
-        for (int i = 0; i < s.hand.length(); i++) {
-            int addLen;
-            char c = s.hand.charAt(i);
-            String newHand = s.hand.substring(0,i) + s.hand.substring(i+1);
-            ArrayList<Integer> positions = findAllPositionsOf("" + c + c, s.board); // "__CC__"
-            positions.addAll(findAllPositionsOf(""+c, s.board)); // "__C__"
+        char[] charArray = s.hand.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            char c = charArray[i];
+            // char from hand can be inserted ANYWHERE on the board (board.len + 1 positions)
+            for (int j = 0; j < s.board.length() + 1; j++) {
+                String newBoard = collapseBoard(new StringBuilder(s.board).insert(j, c).toString());
+                String newHand = new StringBuilder(s.hand).deleteCharAt(i).toString();
+                GameState newState = new GameState(newBoard, newHand, s.depth+1);
 
-
-            // pro vsechny tyhle pozice, vygeneruj board
-            for(int p : positions) {
-                String newBoard = new StringBuilder(s.board).insert(p, c).toString();
-                newBoard = collapseBoard(newBoard);
-                GameState newState = new GameState(newBoard, newHand);
-                graph.add(new ArrayList<>());
-                newState.depth = s.depth+1;
-                int statNum = stateMap.size();
-                stateMap.put(statNum, newState);
-                graph.get(curr).add(statNum); // nbr added to current
-                nbrs.add(statNum);
+                // state "caching"
+                if(!allStates.contains(newState)) {
+                    allStates.add(newState);
+                    int id = stateMap.size();
+                    stateMap.put(id, newState);
+                    graph.add(new ArrayList<>()); // new source for the given id
+                    graph.get(curr).add(id); // adding neighbor to graph
+//                    System.out.println("added " + id + "->["+newBoard+","+newHand+"]");
+                }
             }
         }
-        return nbrs;
     }
 
-    private ArrayList<Integer> findAllPositionsOf(String rgx, String board) {
-        ArrayList<Integer> positions = new ArrayList<>();
-        int i = 0;
-
-        // if there is an occurence, add it to the positions, move fromIndex
-        while(board.indexOf(rgx, i) != -1) {
-            int pos = board.indexOf(rgx, i);
-            positions.add(pos);
-            i = pos + 2;
+    /**
+     * Removes sequence of same consecutive characters of len 3+
+     */
+    public String collapseBoard(String board) {
+        String result;
+        while (true) {
+            int prevLen = board.length();
+            result = removeConsecutive(board);
+            int newLen = result.length();
+            if (newLen == prevLen) break;
+            board = result;
         }
-
-        return positions;
+        return result;
     }
 
+    private String removeConsecutive(String board) {
+        String boardCpy = board + "#";
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> S = new Stack<>();
+        for (char c : boardCpy.toCharArray()) {
+            if (S.empty()) {
+                S.push(c);
+            } else {
+                if (S.peek() != c) { // top is diff
+                    if (S.size() >= 3) {
+                        S = new Stack<>(); // dont pop, just init anew
+                        S.push(c);
+                    }
+                    else { // top is diff and short
+                        while (!S.empty()) {
+                            sb.append(S.pop());
+                        }
+                        S.push(c);
+                    }
+                }
+                else { // top is same
+                    S.push(c);
+                }
+            }
+        }
+        while (!S.empty()) {
+            if (S.peek() == '#') {
+                S.pop();
+            }
+            else {
+                sb.append(S.pop());
+            }
+        }
+        return sb.toString();
+    }
 }
