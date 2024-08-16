ac2409580f48a2093ef91c58baa5bff53230509d
vkuzdas <kuzdavoj@fel.cvut.cz>
2024-08-16 15:09:50 +0200

    LongestUniqSubstring DONE, lets fucking GOOOOO

--- src/LongestUniqSubstring.java
+++ src/LongestUniqSubstring.java
@@ -1,13 +1,14 @@
+import java.util.HashSet;
 import java.util.Hashtable;
 
 public class LongestUniqSubstring {
     public static void main(String[] args) {
         Solution s = new Solution();
-//        System.out.println(s.lengthOfLongestSubstring("abcabcbb")); // 3
-//        System.out.println(s.lengthOfLongestSubstring("bbbbb")); // 1
-//        System.out.println(s.lengthOfLongestSubstring("pwwkew")); // 3
-//        System.out.println(s.lengthOfLongestSubstring("tmmzuxt")); // 5
-//        System.out.println(s.lengthOfLongestSubstring("umvejcuuk")); // 6
+        System.out.println(s.lengthOfLongestSubstring("abcabcbb")); // 3
+        System.out.println(s.lengthOfLongestSubstring("bbbbb")); // 1
+        System.out.println(s.lengthOfLongestSubstring("pwwkew")); // 3
+        System.out.println(s.lengthOfLongestSubstring("tmmzuxt")); // 5
+        System.out.println(s.lengthOfLongestSubstring("umvejcuuk")); // 6
         System.out.println(s.lengthOfLongestSubstring("gaaqfeqlqky")); // 4
     }
 
@@ -19,7 +20,27 @@ public class LongestUniqSubstring {
     // if HT already contains its char, keep shifting s_ptr to the right
     private static class Solution {
         public int lengthOfLongestSubstring(String str) {
+            HashSet<Character> set = new HashSet<>();
+            int f_ptr = 0;
+            int s_ptr = 0;
+            int max = 0;
 
+            while (f_ptr < str.length()) {
+                char f_char = str.charAt(f_ptr);
+                char s_char = str.charAt(s_ptr);
+
+                while(set.contains(f_char)) {
+                    set.remove(s_char);
+                    s_ptr++;
+                    s_char = str.charAt(s_ptr);
+                }
+
+                set.add(f_char);
+                max = Math.max(max, set.size());
+
+                f_ptr++;
+            }
+            return max;
         }
     }
 }
