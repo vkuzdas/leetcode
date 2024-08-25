c3a60e351acf0da4076dd52672bfce67487493a2
vojtechkuzdas <33172723+vkuzdas@users.noreply.github.com>
2024-08-25 21:09:56 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,20 @@
 class Solution {
         public boolean isPalindrome(String s) {
-            StringBuilder stripped_sb = new StringBuilder();
-            StringBuilder reversed_sb = new StringBuilder();
-            for(char c : s.toCharArray()) {
-                if (Character.isAlphabetic(c) || Character.isDigit(c)) {
-                    c = Character.toLowerCase(c);
-                    stripped_sb.insert(stripped_sb.length(), c); // append
-                    reversed_sb.insert(0, c); // prepend
+            int i = 0;
+            int j = s.length()-1;
+            while (i < j) {
+                while (!Character.isAlphabetic(s.charAt(i)) && !Character.isDigit(s.charAt(i)) && i < s.length()-1) {
+                    i++;
                 }
+                while (!Character.isAlphabetic(s.charAt(j)) && !Character.isDigit(s.charAt(j)) && j > 0) {
+                    j--;
+                }
+                if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                    return false;
+                }
+                i++;
+                j--;
             }
-            return stripped_sb.toString().equals(reversed_sb.toString());
+            return true;
         }
 }
