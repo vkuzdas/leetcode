9754f89a68889a556328075684610f7d25cb62a5
vojtechkuzdas <33172723+vkuzdas@users.noreply.github.com>
2024-08-27 22:10:13 +0200

    Time: 0 ms (100%), Space: 41.3 MB (24.12%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -13,23 +13,21 @@
  *     }
  * }
  */
-import java.util.ArrayDeque;
-import java.util.Queue;
 class Solution {
-    public TreeNode invertTree(TreeNode root) {
-        if (root == null) return root;
-        Queue<TreeNode> Q = new ArrayDeque<>();
-        Q.add(root);
-        while (!Q.isEmpty()) {
-            TreeNode curr = Q.poll();
-            if (curr == null) continue;
-            TreeNode temp = curr.left;
-            curr.left = curr.right;
-            curr.right = temp;
-            
-            if (curr.left != null) Q.add(curr.left);
-            if (curr.right != null) Q.add(curr.right);
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null)
+                return root;
+            inv(root);
+            return root;
         }
-        return root;
-    }
+
+        private void inv(TreeNode root) {
+            if (root.left != null || root.right != null ) {
+                TreeNode temp = root.left;
+                root.left = root.right;
+                root.right = temp;
+                invertTree(root.left);
+                invertTree(root.right);
+            }
+        }
 }
