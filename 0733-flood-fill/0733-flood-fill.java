eb9d037cac013e44bf7b2904b977bf1bdc559f9f
vojtechkuzdas <33172723+vkuzdas@users.noreply.github.com>
2024-08-27 21:33:54 +0200

    Time: 1 ms (45.28%), Space: 44.9 MB (51.09%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,31 +1,39 @@
 class Solution {
-        int[][] M;
-        int NEW_COL;
-        int SRC_COL;
-        HashSet<int[]> VISITED;
+        int [][] M;
+        boolean [][] V;
+        int newColor;
+        int startColor;
+        public int[][] floodFill(int[][] image, int sr, int sc, int color) {
 
-        public int[][] floodFill(int[][] image, int r, int c, int color) {
             M = image;
-            NEW_COL = color;
-            VISITED = new HashSet<>();
-            SRC_COL = M[r][c];
-            if (M[r][c] == NEW_COL) return M;
+            startColor = image[sr][sc];
+            newColor = color;
+            V = new boolean[M.length][M[0].length];
 
-            DFS(r, c);
+            if (image[sr][sc] != newColor)
+                BFS(sr, sc);
+
             return M;
         }
 
-        void DFS(int r, int c) {
-            if (M[r][c] != SRC_COL || VISITED.contains(new int[]{r,c})) {
+        private void BFS(int i, int j) {
+            if (M[i][j] == startColor) {
+                M[i][j] = newColor;
+            } else {
                 return;
             }
-            else {
-                M[r][c] = NEW_COL;
-                VISITED.add(new int[]{r,c});
-                if (r > 0)          DFS(r-1, c);
-                if (c > 0)          DFS(r, c-1);
-                if (r < M.length-1) DFS(r+1, c);
-                if (c < M[0].length-1) DFS(r, c+1);
-            }
+            V[i][j] = true;
+
+            if (i < M.length-1 && !V[i+1][j])
+                BFS(i+1,j);
+
+            if (j < M[0].length-1 && !V[i][j+1])
+                BFS(i,j+1);
+
+            if (i > 0 && !V[i-1][j])
+                BFS(i-1,j);
+
+            if (j > 0 && !V[i][j-1])
+                BFS(i,j-1);
         }
 }
