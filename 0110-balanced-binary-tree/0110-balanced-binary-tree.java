b3188d9c390c499238661627592301045c2a4cc9
vojtechkuzdas <33172723+vkuzdas@users.noreply.github.com>
2024-08-28 20:18:39 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,19 +14,31 @@
  * }
  */
 class Solution {
+        boolean balanced = false;
         boolean balanced = true;
         public boolean isBalanced(TreeNode root) {
             getDepth(root);
             return balanced;
         }
 
+        // return depth of given subtree
         private int getDepth(TreeNode root) {
-            if (root == null)
+
+            // if subtree has no children, D = 0
+            if (root == null) {
                 return 0;
-            int L = getDepth(root.left);
-            int R = getDepth(root.right);
-            if(Math.abs(R-L) > 1)
+            }
+
+            // get depth of children
+            int L = getDepth(root.right);
+            int R = getDepth(root.left);
+
+            // if subtrees differ by more than 1
+            if(Math.abs(L-R) > 1) {
                 balanced = false;
-            return Math.max(L,R)+1;
+            }
+
+            // Depth of current tree = max depth of left and right subtree
+            return Math.max(L, R)+1;
         }
 }
