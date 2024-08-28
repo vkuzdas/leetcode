efc0b31412dd3cf39883144d9644e9fb3e0f03e1
vojtechkuzdas <33172723+vkuzdas@users.noreply.github.com>
2024-08-28 20:31:14 +0200

    Time: 17 ms (23.83%), Space: 48.3 MB (8.93%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,58 +1,50 @@
 class Solution {
-        int[][] M;
-        int[][] D;
-        boolean[][]V;
-        Queue<int[]> Q;
-
+        int [][] M;
+        boolean [][] VIS;
+        int [][] D;
         public int[][] updateMatrix(int[][] mat) {
             M = mat;
-
-            V = new boolean[M.length][M[0].length];
+            VIS = new boolean[M.length][M[0].length];
             D = new int[M.length][M[0].length];
-            for (int i = 0; i < M.length; i++) {
-                for (int j = 0; j < M[0].length; j++) {
-                    D[i][j] = Integer.MAX_VALUE;
-                }
-            }
 
-            Q = new ArrayDeque<>();
+            Queue<int[]> Q = new ArrayDeque<>();
+
             for (int i = 0; i < M.length; i++) {
                 for (int j = 0; j < M[0].length; j++) {
                     if (M[i][j] == 0) {
-                        Q.add(new int[]{i,j}); // add all zeros to Q
-                        V[i][j] = true;
+                        Q.add(new int[]{i,j});
+                        VIS[i][j] = true;
                         D[i][j] = 0;
                     }
                 }
             }
 
             while (!Q.isEmpty()) {
-                int[] top = Q.poll();
+                int [] top = Q.poll();
                 int i = top[0];
                 int j = top[1];
 
-                if (i < M.length-1 && !V[i+1][j]) {
-                    D[i+1][j] = D[i][j]+1;
-                    V[i+1][j] = true;
-                    Q.add(new int[] {i+1,j});
+                if (j > 0 && !VIS[i][j-1]) {
+                    D[i][j - 1] = D[i][j] + 1;
+                    Q.add(new int[]{i,j-1});
+                    VIS[i][j-1] = true;
                 }
-                if (j < M[0].length-1 && !V[i][j+1]) {
-                    D[i][j+1] = D[i][j]+1;
-                    V[i][j+1] = true;
-                    Q.add(new int[] {i,j+1});
+                if (j < M[0].length-1 && !VIS[i][j+1]) {
+                    D[i][j + 1] = D[i][j] + 1;
+                    Q.add(new int[]{i,j+1});
+                    VIS[i][j+1] = true;
                 }
-                if (i > 0 && !V[i-1][j]) {
-                    D[i-1][j] = D[i][j]+1;
-                    V[i-1][j] = true;
-                    Q.add(new int[] {i-1,j});
+                if (i > 0 && !VIS[i-1][j]) {
+                    D[i - 1][j] = D[i][j] + 1;
+                    Q.add(new int[]{i-1,j});
+                    VIS[i-1][j] = true;
                 }
-                if (j > 0 && !V[i][j-1]) {
-                    D[i][j-1] = D[i][j]+1;
-                    V[i][j-1] = true;
-                    Q.add(new int[] {i,j-1});
+                if (i < M.length-1 && !VIS[i+1][j]) {
+                    D[i + 1][j] = D[i][j] + 1;
+                    Q.add(new int[]{i+1,j});
+                    VIS[i+1][j] = true;
                 }
             }
-
             return D;
         }
 }
