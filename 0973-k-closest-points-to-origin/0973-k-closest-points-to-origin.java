307bd343ced05a67b3ad566d5a4f1df16e041bfd
vojtechkuzdas <33172723+vkuzdas@users.noreply.github.com>
2024-08-28 21:53:27 +0200

    Time: 31 ms (54.35%), Space: 52 MB (64.19%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -2,37 +2,28 @@ class Solution {
         class Point {
             int x;
             int y;
-            int dist;
+            int d;
 
-            public Point(int x, int y, int dist) {
+            public Point(int x, int y, int d) {
                 this.x = x;
                 this.y = y;
-                this.dist = dist;
+                this.d = d;
             }
         }
         public int[][] kClosest(int[][] points, int k) {
-            List<Point> pts = new ArrayList<>();
-            for (int i = 0; i < points.length; i++) {
-                pts.add(new Point(points[i][0], points[i][1], (int) (Math.pow(points[i][0],2) + Math.pow(points[i][1],2))));
+            List<Point> points_str = new ArrayList<>();
+            for (int[] pt : points) {
+                int d = pt[0]*pt[0] + pt[1]*pt[1]; 
+                points_str.add(new Point(pt[0], pt[1], d));
             }
-            List<Point> newpts = pts.stream().sorted(Comparator.comparingInt(value -> value.dist)).limit(k).toList();
-            int[][] ret = new int[k][2];
-            for(int i = 0; i < newpts.size(); i++) {
-                ret[i][0] = newpts.get(i).x;
-                ret[i][1] = newpts.get(i).y;
+            
+            int[][] result = new int[k][2];
+            points_str.sort(Comparator.comparingInt(c -> c.d));
+            
+            for (int i = 0; i < k; i++) {
+                result[i][0] = points_str.get(i).x;                
+                result[i][1] = points_str.get(i).y;
             }
-            return ret;
-//            HashMap<Integer, int[]> pt_dist = new HashMap<>();
-//            for (int i = 0; i < points.length; i++) {
-//                int[] pt = points[i];
-//                int dist = pt[0]^2 + pt[1]^2;
-//                pt_dist.put(dist, pt);
-//            }
-//            List<Integer> keys = pt_dist.keySet().stream().sorted().limit(k).toList();
-//            int[][] kClosest = new int[keys.size()][];
-//            for (int i = 0; i < keys.size(); i++) {
-//                kClosest[i] = pt_dist.get(keys.get(i));
-//            }
-//            return kClosest;
+            return result;
         }
 }
