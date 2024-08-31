7ec7306a55f2fefcbec6003394afa7f78a024e30
vojtechkuzdas <33172723+vkuzdas@users.noreply.github.com>
2024-08-31 17:47:48 +0200

    Time: 0 ms (100%), Space: 45.8 MB (22.94%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,27 +1,43 @@
 class Solution {
-        int [] A;
-        int t;
         public int search(int[] nums, int target) {
-            A = nums;
-            t = target;
-            return bs(0,A.length-1);
+            int i = 0;
+            int j = nums.length-1;
+            int m = i+(j-i)/2; // prevent overflow
+
+            if (target == nums[m]) {
+                return m;
+            }
+            else if (target > nums[m]) {
+                return bs(m, j, nums, target);
+            }
+            else /*if (target < nums[m])*/ {
+                return bs(i, m, nums, target);
+            }
         }
 
-        private int bs(int i, int j) {
-            int m = (i+j)/2;
-            if (A[m] == t) return m;
-            if (i==j)
-                return A[i] == t ? i : -1;
-            if (j-i == 1)
-                if (A[i] == t) return i;
-                else if (A[j] == t) return j;
-                else return -1;
-
-            if (A[m] > t) {
-                return bs(i, m);
-            } else if (A[m] < t) {
-                return bs(m, j);
+        private int bs(int i, int j, int[] nums, int target) {
+            if (j == i) {
+                return target == nums[i] ? i : -1;
             }
-            return -1;
+            if (j-i == 1) {
+                if (nums[j] == target) {
+                    return j;
+                } else if (nums[i] == target) {
+                    return i;
+                } else {
+                    return -1;
+                }
+            }
+
+            int m = i+(j-i)/2;
+            if (target == nums[m]) {
+                return m;
+            }
+            else if (target > nums[m]) {
+                return bs(m, j, nums, target);
+            }
+            else /*if (target < nums[m])*/ {
+                return bs(i, m, nums, target);
+            }
         }
 }
