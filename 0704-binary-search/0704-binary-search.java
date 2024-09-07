cb4289b6da4fea1f70c409a939647b017c5ea03e
vojtechkuzdas <33172723+vkuzdas@users.noreply.github.com>
2024-09-07 18:56:37 +0200

    Time: 0 ms (100%), Space: 45.4 MB (79.98%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,43 +1,17 @@
 class Solution {
         public int search(int[] nums, int target) {
-            int i = 0;
-            int j = nums.length-1;
-            int m = i+(j-i)/2; // prevent overflow
-
-            if (target == nums[m]) {
-                return m;
-            }
-            else if (target > nums[m]) {
-                return bs(m, j, nums, target);
-            }
-            else /*if (target < nums[m])*/ {
-                return bs(i, m, nums, target);
-            }
-        }
-
-        private int bs(int i, int j, int[] nums, int target) {
-            if (j == i) {
-                return target == nums[i] ? i : -1;
-            }
-            if (j-i == 1) {
-                if (nums[j] == target) {
-                    return j;
-                } else if (nums[i] == target) {
-                    return i;
-                } else {
-                    return -1;
+            int l = 0;
+            int h = nums.length-1;
+            while(l<=h) {
+                int m = l+(h-l)/2;
+                if (target == nums[m]) return m;
+                if (target < nums[m]) {
+                    h = m - 1;
+                }
+                else if (target > nums[m]) {
+                    l = m + 1;
                 }
             }
-
-            int m = i+(j-i)/2;
-            if (target == nums[m]) {
-                return m;
-            }
-            else if (target > nums[m]) {
-                return bs(m, j, nums, target);
-            }
-            else /*if (target < nums[m])*/ {
-                return bs(i, m, nums, target);
-            }
+            return -1;
         }
 }
