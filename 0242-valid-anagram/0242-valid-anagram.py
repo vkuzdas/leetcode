865fba01bb5c9c8b41cf5c92a6a44269c477ed52
vojtechkuzdas <33172723+vkuzdas@users.noreply.github.com>
2025-04-30 07:28:58 +0200

    Time: 3 ms (98.34%), Space: 18 MB (49.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,22 +1,12 @@
+from typing import Counter
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         """
         Given two strings s and t, 
         return true if t is an anagram of s, and false otherwise.
         """
-        if len(s) != len(t):
-            return False
-        dict1 = {}
-        dict2 = {}
-        for i in range(0, len(s)):
-            if s[i] in dict1:
-                dict1[s[i]] = dict1[s[i]] + 1
-            else:
-                dict1[s[i]] = 1
-
-            if t[i] in dict2:
-                dict2[t[i]] = dict2[t[i]] + 1
-            else:
-                dict2[t[i]] = 1
-
+        dict1 = Counter(s)
+        dict2 = Counter(t)
         return dict1 == dict2
